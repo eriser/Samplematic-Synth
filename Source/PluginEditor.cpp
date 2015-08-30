ff905753202d014595d8bf9931b47c3d7d04ef0a
Karl Hiner <karl.hiner@gmail.com>
2015-08-30 00:39:44 -0700

    Working skeleton of VST with single knob.  Just plays a sawtooth for now

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,136 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Introjucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Introjucer version: 3.2.0
+
+  ------------------------------------------------------------------------------
+
+  The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+//[Headers] You can add your own extra header files here...
+//[/Headers]
+
+#include "PluginEditor.h"
+
+
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
+
+//==============================================================================
+SamplematicSynthAudioProcessorEditor::SamplematicSynthAudioProcessorEditor (SamplematicSynthAudioProcessor *ownerFilter)
+    : AudioProcessorEditor(ownerFilter)
+{
+    //[Constructor_pre] You can add your own custom stuff here..
+    //[/Constructor_pre]
+
+    addAndMakeVisible (slider = new Slider ("new slider"));
+    slider->setRange (0, 1, 0.1);
+    slider->setSliderStyle (Slider::Rotary);
+    slider->setTextBoxStyle (Slider::TextBoxBelow, false, 80, 20);
+    slider->addListener (this);
+
+
+    //[UserPreSize]
+    //[/UserPreSize]
+
+    setSize (400, 400);
+
+
+    //[Constructor] You can add your own custom stuff here..
+    //[/Constructor]
+}
+
+SamplematicSynthAudioProcessorEditor::~SamplematicSynthAudioProcessorEditor()
+{
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+    slider = nullptr;
+
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
+}
+
+//==============================================================================
+void SamplematicSynthAudioProcessorEditor::paint (Graphics& g)
+{
+    //[UserPrePaint] Add your own custom painting code here..
+    //[/UserPrePaint]
+
+    g.fillAll (Colours::white);
+
+    //[UserPaint] Add your own custom painting code here..
+    //[/UserPaint]
+}
+
+void SamplematicSynthAudioProcessorEditor::resized()
+{
+    //[UserPreResize] Add your own custom resize code here..
+    //[/UserPreResize]
+
+    slider->setBounds (100, 100, 200, 200);
+    //[UserResized] Add your own custom resize handling here..
+    //[/UserResized]
+}
+
+void SamplematicSynthAudioProcessorEditor::sliderValueChanged (Slider* sliderThatWasMoved)
+{
+    //[UsersliderValueChanged_Pre]
+    //[/UsersliderValueChanged_Pre]
+
+    if (sliderThatWasMoved == slider)
+    {
+        //[UserSliderCode_slider] -- add your slider handling code here..
+        //[/UserSliderCode_slider]
+    }
+
+    //[UsersliderValueChanged_Post]
+    //[/UsersliderValueChanged_Post]
+}
+
+
+
+//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
+//[/MiscUserCode]
+
+
+//==============================================================================
+#if 0
+/*  -- Introjucer information section --
+
+    This is where the Introjucer stores the metadata that describe this GUI layout, so
+    make changes in here at your peril!
+
+BEGIN_JUCER_METADATA
+
+<JUCER_COMPONENT documentType="Component" className="SamplematicSynthAudioProcessorEditor"
+                 componentName="" parentClasses="public AudioProcessorEditor"
+                 constructorParams="SamplematicSynthAudioProcessor *ownerFilter"
+                 variableInitialisers="AudioProcessorEditor(ownerFilter)" snapPixels="8"
+                 snapActive="1" snapShown="1" overlayOpacity="0.330" fixedSize="0"
+                 initialWidth="400" initialHeight="400">
+  <BACKGROUND backgroundColour="ffffffff"/>
+  <SLIDER name="new slider" id="6ed9fc54852baa51" memberName="slider" virtualName=""
+          explicitFocusOrder="0" pos="100 100 200 200" min="0" max="1"
+          int="0.10000000000000000555" style="Rotary" textBoxPos="TextBoxBelow"
+          textBoxEditable="1" textBoxWidth="80" textBoxHeight="20" skewFactor="1"/>
+</JUCER_COMPONENT>
+
+END_JUCER_METADATA
+*/
+#endif
+
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
---
+++ Source/PluginEditor.h
@@ -0,0 +1,71 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Introjucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Introjucer version: 3.2.0
+
+  ------------------------------------------------------------------------------
+
+  The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#ifndef __JUCE_HEADER_12290EE09574D8BD__
+#define __JUCE_HEADER_12290EE09574D8BD__
+
+//[Headers]     -- You can add your own extra header files here --
+#include "JuceHeader.h"
+#include "PluginProcessor.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Introjucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class SamplematicSynthAudioProcessorEditor  : public AudioProcessorEditor,
+                                              public SliderListener
+{
+public:
+    //==============================================================================
+    SamplematicSynthAudioProcessorEditor (SamplematicSynthAudioProcessor *ownerFilter);
+    ~SamplematicSynthAudioProcessorEditor();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g);
+    void resized();
+    void sliderValueChanged (Slider* sliderThatWasMoved);
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
+
+    //==============================================================================
+    ScopedPointer<Slider> slider;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SamplematicSynthAudioProcessorEditor)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
+#endif   // __JUCE_HEADER_12290EE09574D8BD__
